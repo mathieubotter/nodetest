afdc4c0ebce6be9e8dc7343ca3ef0fcf51b9b7d0
Mathieu Botter <botter.mathieu@gmail.com>
2011-07-12 13:04:10 +0000

    added response to requestHandlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,13 @@
 function index() {
     console.log("Request handler 'index' was called.");
+    
+    return 'Hello Index';
 }
 
 function test() {
     console.log("Request handler 'test' was called.");
+    
+    return 'Hello Test';
 }
 
 exports.index = index;
