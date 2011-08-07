705362aafe300f54ed994a4c76d44d04d8641af6
Mathieu Botter <botter.mathieu@gmail.com>
2011-08-07 19:45:12 +0000

    Handling POST requests

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,54 @@
-function index(response) {
+var querystring = require("querystring");
+
+function index(response, postData) {
     console.log("Request handler 'index' was called.");
     
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write('Hello Index');
+    var body = '<!DOCTYPE html>\n'+
+        '<html lang="fr">\n'+
+        '<head>\n'+
+        '   <meta charset="utf-8" />\n'+
+        '   <title>NodeTest</title>\n'+
+        '</head>\n'+
+        '<body>\n'+
+        '   <h1>NodeTest</h1>\n'+
+        '   <p>Bienvenue sur le site de test de nodejs.</p>\n'+
+        '   <p>Page de test : <a href="/test">test</a></p>\n'+
+        '</body>\n'+
+        '</html>';
+    
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
     response.end();
 }
 
-function test(response) {
+function test(response, postData) {
     console.log("Request handler 'test' was called.");
+    var data = '';
     
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello Test');
+    if (postData === '')
+        data = "rien";
+    else
+        data = querystring.parse(postData).text;
+    
+    var body = '<!DOCTYPE html>\n'+
+        '<html lang="fr">\n'+
+        '<head>\n'+
+        '   <meta charset="utf-8" />\n'+
+        '   <title>Formulaire - NodeTest</title>\n'+
+        '</head>\n'+
+        '<body>\n'+
+        '   <p><a href="/">Retour</a></p>\n'+
+        '   <form action="/test" method="post">\n'+
+        '       <textarea name="text">Votre message</textarea>\n'+
+        '       <hr />\n'+
+        '       <input type="submit" value="Envoyer" />\n'+
+        '   </form>\n'+
+        '   <p>Vous avez envoyé : ' + data + '</p>\n'+
+        '</body>\n'+
+        '</html>';
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(body);
     response.end();
 }
 
