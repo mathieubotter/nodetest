f9a0191a47e7c5cbee9ab91908d04a795209bd54
Mathieu Botter <mathieubotter@outlook.com>
2013-05-12 19:25:17 -0400

    Add Gruntfile.js file

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  // Load the plugin that provides the "" task.
+  //grunt.loadNpmTasks('');
+
+  // Default task(s).
+  //grunt.registerTask('default', ['']);
+
+};
