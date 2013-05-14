12095dbe512e0ca7fe80cc8e20acca3e5d3f7f13
Mathieu Botter <mathieubotter@outlook.com>
2013-05-13 23:54:49 -0400

    Add jshint task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,18 @@
-"use strict";
-
 module.exports = function(grunt) {
+  'use strict';
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', 'test/*.js']
+    }
   });
 
-  // Load the plugin that provides the "" task.
-  //grunt.loadNpmTasks('');
+  // Load the plugin that provides the "JSHint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  //grunt.registerTask('default', ['']);
+  grunt.registerTask('default', ['jshint']);
 
 };
