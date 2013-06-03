8dcd4442d9e9e6a1126362a49aa311e9b1fa6886
Mathieu Botter <mathieubotter@outlook.com>
2013-06-03 01:05:01 -0400

    Show all todo items

--- routes/todo.js
+++ routes/todo.js
@@ -1,15 +1,23 @@
+ var mongoose = require('mongoose');
+ var Todo = mongoose.model('Todo');
 
 /*
  * GET todo page.
  */
 
- var mongoose = require('mongoose');
- var Todo = mongoose.model('Todo');
-
 exports.index = function(req, res){
-  res.render('todo', { title: 'Todo' });
+	Todo.find(function (err, todos, count){
+		res.render('todo', {
+			title: 'Todo',
+			todos: todos
+		});
+	});
 };
 
+/*
+ * POST new todo.
+ */
+ 
 exports.add = function(req, res){
 	new Todo({
 		content    : req.body.content,
