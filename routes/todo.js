4bf83343468d660d8a0e273af17ebfd7ae2b19aa
Mathieu Botter <mathieubotter@outlook.com>
2013-06-04 22:15:01 -0400

    Add multiple users and error handling

--- routes/todo.js
+++ routes/todo.js
@@ -1,13 +1,18 @@
  var mongoose = require('mongoose');
  var Todo = mongoose.model('Todo');
+ var utils = require('connect').utils;
 
 // GET todo items
-exports.index = function(req, res){
-  if(req.params.id !== null){
+exports.index = function(req, res, next){
+  var user_id = req.cookies ? req.cookies.user_id : undefined;
+
+  if(req.params.id !== undefined){
     Todo
-      .find()
+      .find({ user_id: user_id })
       .sort('-updated_at')
       .exec(function (err, todos, count){
+        if(err) return next(err);
+
         res.render('todo', {
           title: 'Edit todo',
           todos: todos,
@@ -16,9 +21,11 @@ exports.index = function(req, res){
       });
   } else {
     Todo
-      .find()
+      .find({ user_id: user_id })
       .sort('-updated_at')
-      .exec(function (err, todos, count){
+      .exec(function (err, todos){
+        if(err) return next(err);
+
         res.render('todo', {
           title: 'Todo',
           todos: todos,
@@ -29,31 +36,60 @@ exports.index = function(req, res){
 };
 
 // POST new todo
-exports.add = function(req, res){
+exports.add = function(req, res, next){
   new Todo({
+    user_id: req.cookies.user_id,
     content: req.body.content,
     updated_at: Date.now()
   }).save(function(err, todo, count){
+    if(err) return next(err);
+
     res.redirect('/todo');
   });
 };
 
 // Edit todo item
-exports.edit = function(req, res){
+exports.edit = function(req, res, next){
   Todo.findById(req.params.id, function(err, todo){
+    var user_id = req.cookies ? req.cookies.user_id : undefined;
+
+    if (todo.user_id !== user_id){
+      return utils.forbidden(res);
+    }
+
     todo.content = req.body.content;
     todo.updated_at = Date.now();
     todo.save(function(err, todo, count){
+      if(err) return next(err);
+
       res.redirect('/todo');
     });
   });
 };
 
 // DELETE todo item
-exports.destroy = function(req, res){
+exports.destroy = function(req, res, next){
   Todo.findById(req.params.id, function (err, todo){
+    var user_id = req.cookies ? req.cookies.user_id : undefined;
+
+    if(todo.user_id !== user_id){
+      return utils.forbidden(res);
+    }
+
     todo.remove(function(err, todo){
+      if(err) return next(err);
+
       res.redirect('/todo');
     });
   });
+};
+
+exports.current_user = function(req, res, next){
+  var user_id = req.cookies ? req.cookies.user_id : undefined;
+
+  if(!user_id){
+    res.cookie('user_id', utils.uid(32));
+  }
+
+  next();
 };
