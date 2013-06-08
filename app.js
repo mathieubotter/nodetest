76b3188cf943eb1740e18843e7b48d41a528f694
Mathieu Botter <mathieubotter@outlook.com>
2013-06-07 23:59:55 -0400

    Change configuration settings

--- app.js
+++ app.js
@@ -19,24 +19,31 @@ var app = express();
 app.engine('ejs', engine);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('secret'));
-app.use(express.session());
-app.use('/todo', todo.current_user);
-app.use(app.router);
-app.use(require('stylus').middleware(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('secret'));
+    app.use(express.session());
+    app.use('/todo', todo.current_user);
+    app.use(app.router);
+    app.use(require('stylus').middleware(__dirname + '/public'));
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.configure('development', function(){
+    app.use(express.errorHandler());
+});
+
+// production
+app.configure('production', function(){
+    //...
+});
 
 app.get('/', home.index);
 app.get('/about', about.index);
